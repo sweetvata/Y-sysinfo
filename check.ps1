dd6a6dbaf46cfdbbbe2f4cded51df451c7f01bba
sweety <babefreakja@gmail.com>
2026-06-21 22:52:43 +0300

    Add files via upload

--- check.ps1
+++ check.ps1
@@ -1,4 +1,4 @@
-f (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Write-Host "[ERROR] Run as Administrator!" -ForegroundColor Red
     exit
 }
